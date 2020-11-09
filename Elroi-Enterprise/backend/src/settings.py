2b14013c262a236624a96b2949d5c7cd0aca979f
0xjeremdev <jeremy.pierce221@gmail.com>
2020-11-09 23:32:21 +0000

    update postgre port

--- Elroi-Enterprise/backend/src/settings.py
+++ Elroi-Enterprise/backend/src/settings.py
@@ -118,7 +118,7 @@ DATABASES = {
         'USER': 'postgres',
         'PASSWORD': 'postgres',
         'HOST': 'localhost',
-        'PORT': '5433',
+        'PORT': '5432',
     }
 }
 STATUSES = (
