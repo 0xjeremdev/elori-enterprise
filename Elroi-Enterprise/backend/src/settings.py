5aba74635a5aaeb28635bbcef555ee03e032ef1d
0xjeremdev <jeremy.pierce221@gmail.com>
2020-11-09 23:23:43 +0000

    update static-root url

--- Elroi-Enterprise/backend/src/settings.py
+++ Elroi-Enterprise/backend/src/settings.py
@@ -207,7 +207,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
