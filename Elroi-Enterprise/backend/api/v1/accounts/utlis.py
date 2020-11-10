4276209ede9fce908ef6d5e54741f88b76e5c1fe
0xjeremdev <jeremy.pierce221@gmail.com>
2020-11-10 20:09:25 +0000

    update account utils

--- Elroi-Enterprise/backend/api/v1/accounts/utlis.py
+++ Elroi-Enterprise/backend/api/v1/accounts/utlis.py
@@ -4,18 +4,18 @@ from django.core.mail import EmailMessage
 from sendgrid import SendGridAPIClient
 from sendgrid.helpers.mail import Mail
 from dotenv import load_dotenv, find_dotenv
+from django.conf import settings
 
 class SendUserEmail:
     @staticmethod
     def send_email(data):
-        load_dotenv(find_dotenv())
         message = Mail(
-            from_email=os.getenv('DEFAULT_FROM_EMAIL'),
+            from_email=settings.DEFAULT_FROM_EMAIL,
             to_emails=data['to_email'],
             subject=data['email_subject'],
             html_content=data['email_body'])
         try:
-            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+            sg = SendGridAPIClient(settings.SENDGRID_API_KEY)
             response = sg.send(message)
             print(response.status_code)
         except Exception as e:
