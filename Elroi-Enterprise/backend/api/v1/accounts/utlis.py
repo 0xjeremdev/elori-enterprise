29972ed739a491e5f562d1b3ab4d62266a4c3de2
0xjeremdev <jeremy.pierce221@gmail.com>
2020-11-10 19:01:56 +0000

    update account utils

--- Elroi-Enterprise/backend/api/v1/accounts/utlis.py
+++ Elroi-Enterprise/backend/api/v1/accounts/utlis.py
@@ -1,34 +1,16 @@
 import pyotp
-import os
-from django.core.mail import EmailMessage
-from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
-from dotenv import load_dotenv, find_dotenv
-load_dotenv(find_dotenv())
+from django.core.mail import send_mail
+from django.conf import settings
 
 class SendUserEmail:
     @staticmethod
     def send_email(data):
-        # email = EmailMessage(
-        #     subject=data['email_subject'],
-        #     body=data['email_body'],
-        #     to=[data['to_email']]
-        # )
-        # email.content_subtype = 'html'
-        # email.send()
-        message = Mail(
-            from_email=os.getenv('DEFAULT_FROM_EMAIL'),
-            to_emails=data['to_email'],
-            subject=data['email_subject'],
-            html_content=data['email_body'])
-        try:
-            sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-            response = sg.send(message)
-            print(response.status_code)
-            print(response.body)
-            print(response.headers)
-        except Exception as e:
-            print(e)
+        send_mail(
+            data['email_subject'],
+            data['email_body'],
+            settings.DEFAULT_FROM_EMAIL,
+            [data['to_email']]
+        )
 
 
 def generate_verification_code():
