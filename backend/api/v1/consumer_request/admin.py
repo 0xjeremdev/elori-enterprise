2c682619d0b9416aa6d6365f85342cfa475bb0db
0xjeremdev <jeremy.pierce221@gmail.com>
2021-01-26 07:49:14 +0900

    add consumer request admin

--- backend/api/v1/consumer_request/admin.py
+++ backend/api/v1/consumer_request/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from django import forms
+from .models import ConsumerRequest
 # Register your models here.
+
+
+@admin.register(ConsumerRequest)
+class ConsumerRequestAdmin(admin.ModelAdmin):
+    list_display = ("email", )
