2295ce77a2b6ba741429757ef969f23d886b868c
0xjeremdev <jeremy.pierce221@gmail.com>
2021-01-26 08:26:23 +0900

    update

--- backend/api/v1/enterprise/admin.py
+++ backend/api/v1/enterprise/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django import forms
+from .models import EnterpriseEmailType
+# Register your models here.
+
+
+@admin.register(EnterpriseEmailType)
+class EnterpriseEmailTypeAdmin(admin.ModelAdmin):
+    list_display = ("email_type", )
+
 
 # Register your models here.
